63b7dabf3be8c70970e1d874f2ee95b01c71a600
Shanmugaraja * <shanmugaraja.1@globallogic.com>
2022-09-30 15:54:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Map from "./components/Map";
 
 function App() {
+  const [isShow, setIsShow] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Crime Density </h1>
+      <button onClick={() => setIsShow(!isShow)}>
+        Click to {isShow ? "Close" : "Open"} the Map
+      </button>
+      <div>{isShow && <Map />}</div>
     </div>
   );
 }
